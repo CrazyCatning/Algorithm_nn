d8e8eb8ef62c16a726203bac3a2bbf1de7c2338e
snin <shuxiaoningak@163.com>
2024-03-28 15:42:31 +0800

    有点乱,重新开始

---
+++ tmp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long i64;
+typedef pair<int, int> PII;
+
+vector<int> alls;
+vector<PII> add, query;
+
+const int N = 100010;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "hello world" << endl;
+    return 0;
+} 
